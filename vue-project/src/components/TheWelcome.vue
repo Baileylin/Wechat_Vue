f0c2a276c663f4e0617e9e9a0838c339ed1596d6
Baileylin <baileylinhfi@gmail.com>
2024-06-04 01:33:43 -0400

    update

---
+++ vue-project/src/components/TheWelcome.vue
@@ -0,0 +1,21 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+
+function make() {
+  fetch('https://api.coindesk.com/v1/bpi/currentprice.json').then(response => {
+    return response.json()
+  }).then(data => {
+    console.log(data)
+  })
+  }
+</script>
+
+<template>
+  <a href='https://api.coindesk.com/v1/bpi/currentprice.json' @click='make'>Hello World!</a> 
+  
+</template>
